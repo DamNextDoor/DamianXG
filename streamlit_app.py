55e4ac37b4b200cdfef8120e8ecb2cdd7165afdb
DamNextDoor <damiansheridan07@gmail.com>
2025-06-13 20:38:27 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,4 @@ st.title("WELCOME to Damian Page!")
 st.write(
     "hej!!!🥳"
 )
-
-https://github.com/DamNextDoor/DamianXG/blob/main/IMG-20250612-WA0145.jpg
+st.image("https://github.com/DamNextDoor/DamianXG/blob/main/IMG-20250612-WA0145.jpg",width=200)
